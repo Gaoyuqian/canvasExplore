b6c323f633c95644d020b6ac4434094b14ac9964
Yuqian Gao <yuqian.gao@mljr.com>
2017-04-27 15:24:24 +0800

    雏形

--- 饼图/bingA.js
+++ 饼图/bingA.js
@@ -9,20 +9,17 @@
 //画一个饼图
 
 
-
-
 var bArray = [];
-var colorArray = ['red','orange','yellow','black','grey','green']
+var colorArray = ['red', 'orange', 'yellow', 'black', 'grey', 'green']
 var canvas = document.getElementById('bingA');
 var g = canvas.getContext('2d');
 
 
-
-function getBing(x,y,radius,color,value,name){
-    this.x=x;
-    this.y=y;
-    this.radius=radius;
-    this.color=color;
+function getBing(x, y, radius, color, value, name) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
     this.value = value;
     this.name = name;
 }
@@ -31,25 +28,24 @@ function getBing(x,y,radius,color,value,name){
 
 var total = 0;
 var temp = 0;//负责接收上一个
-function drawBing(arg){
+function drawBing(arg) {
     console.log(typeof arg);
 
-    if(typeof arg == 'object'){
-        for(let i of arg){
+    if (typeof arg == 'object') {
+        for (let i of arg) {
             total += i.value;
-            bArray.push(new getBing(200,200,100,colorArray[colorArray.length-1], i.value, i.name));
+            bArray.push(new getBing(200, 200, 100, colorArray[colorArray.length - 1], i.value, i.name));
             colorArray.pop();
-            console.log(colorArray)
         }
     }
-    for(let i in bArray) {
+    for (let i in bArray) {
         g.beginPath();
-        g.arc(  bArray[i].x,   bArray[i].y,   bArray[i].radius, temp * Math.PI,((bArray[i].value/total*2)+temp)* Math.PI, false);
-        g.lineTo( bArray[i].x,   bArray[i].y);
+        g.arc(bArray[i].x, bArray[i].y, bArray[i].radius, temp * Math.PI, ((bArray[i].value / total * 2) + temp) * Math.PI, false);
+        g.lineTo(bArray[i].x, bArray[i].y);
         g.fillStyle = bArray[i].color;
         g.fill();
-        temp  = (bArray[i].value/total*2)+temp;
-        if(g.isPointInPath(200,240)){
+        temp = (bArray[i].value / total * 2) + temp;
+        if (g.isPointInPath(200, 240)) {//判断在哪个扇形内
             console.log(bArray[i])
         }
     }
@@ -61,7 +57,6 @@ function randomFromTo(from, to) {
 }//获取随机坐标
 
 
-
-window.onload = function(){
-    drawBing([{'name':'测试1','value':3333},{'name':'测试3','value':1112},{'name':'测试3','value':2342}])
+window.onload = function () {
+    drawBing([{'name': '测试1', 'value': 3333}, {'name': '测试3', 'value': 1112}, {'name': '测试3', 'value': 2342}])
 }
