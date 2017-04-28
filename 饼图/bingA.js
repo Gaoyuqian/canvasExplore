5da488d98ca75e75cb3dc13b1449fe0920aa9671
Yuqian Gao <yuqian.gao@mljr.com>
2017-04-28 18:51:31 +0800

    准备添加文字

--- 饼图/bingA.js
+++ 饼图/bingA.js
@@ -10,11 +10,11 @@
 
 
 var bArray = [];
+var lArray = [];
+var tArray = [];
 var colorArray = ['red', 'orange', 'yellow', 'black', 'grey', 'green']
 var canvas = document.getElementById('bingA');
 var g = canvas.getContext('2d');
-
-
 function getBing(x, y, radius, color, value, name) {
     this.x = x;
     this.y = y;
@@ -27,13 +27,39 @@ function getBing(x, y, radius, color, value, name) {
     this.newY = 0;
 }
 
+
+function getLine(x,y,deg,length,color){
+
+    this.Bx = x;
+    this.By = y;
+    this.NBx = 0;
+    this.NBy = 0;
+    this.deg = deg;//角度
+    this.length = length;//长度
+    this.Ex = x +length*Math.cos(deg);
+    this.Ey = y +length*Math.sin(deg);
+    this.color = color;
+}
+
+
+
+function drawLine(deg){
+
+}
+
+
 //接受对象为参数
+function drawText(){
+
+}
 
 var total = 0;
-var temp = 0;//负责接收上一个
 function drawBing(arg) {
     var temp = 0;//负责接收上一个
     g.clearRect(0, 0, 1000, 1000);
+
+
+
     if (typeof arg == 'object') {
         for (let i of arg) {
             total += i.value;
@@ -41,6 +67,11 @@ function drawBing(arg) {
             colorArray.pop();
         }
     }
+
+
+
+
+
     for (let i in bArray) {
         g.beginPath();
         g.arc(bArray[i].newX || bArray[i].x, bArray[i].newY || bArray[i].y, bArray[i].radius, temp * Math.PI, ((bArray[i].value / total * 2) + temp) * Math.PI, false);
@@ -50,7 +81,25 @@ function drawBing(arg) {
         bArray[i].begin = temp * 180;
         bArray[i].end = ((bArray[i].value / total * 2) + temp) * 180;
         temp = (bArray[i].value / total * 2) + temp;
+
+
+
+
+        //drawLine
+        var deg = ( bArray[i].end-parseInt( bArray[i].end -  bArray[i].begin) / 2)*Math.PI/180;
+        if(lArray.length<bArray.length){
+            lArray.push(new getLine(bArray[i].x,bArray[i].y,deg,150,bArray[i].color));
+        }
+        var line  = lArray[i];
+        g.beginPath();
+        g.moveTo(line.NBx||line.Bx,line.NBy||line.By);
+        g.lineTo(line.Ex,line.Ey);
+        g.strokeStyle = line.color;
+        g.stroke();
     }
+
+
+
 }
 
 
@@ -66,34 +115,59 @@ function moveEvent(e) {
     for (var i in bArray) {
         //判断在哪个圆内
         var bing = bArray[i];
-        if (bing.isSelected) {
+        var line = lArray[i];
+
+
+        if (bing.isSelected) {//计算点到扇形圆心的距离
             var distance = Math.sqrt(Math.pow(bing.newX - mouseX, 2) + Math.pow(bing.newY - mouseY, 2))
         } else {
             var distance = Math.sqrt(Math.pow(bing.x - mouseX, 2) + Math.pow(bing.y - mouseY, 2))
         }
+
+
         if (distance <= bing.radius) {
             var XformO = mouseX - (bing.newX || bing.x);
             var YformO = mouseY - (bing.newX || bing.y);
             var angle;
             var angleX = Math.atan(XformO / YformO) * 180 / Math.PI;
+
+
+
             if (YformO >= 0 && XformO >= 0 || YformO >= 0 && XformO < 0) {//第一、二象限
                 angle = 90 - angleX;
             } else if (YformO <= 0 && XformO >= 0 || YformO <= 0 && XformO < 0) {//第三、四象限
                 angle = 90 - angleX + 180
             }
-            if (angle >= bing.begin && (angle <= bing.end)) {
+
+
+
+            if (angle >= bing.begin && (angle <= bing.end)) {//判断点击区域是否在扇形区域内
                 bing.isSelected = !bing.isSelected;
 
             } else {
                 bing.isSelected = false;
             }
-            if (bing.isSelected) {
+
+
+
+
+            if (bing.isSelected) {//选中后重新画
                 bing.newX = bing.x + 20 * Math.cos((bing.end-parseInt(bing.end - bing.begin) / 2)*Math.PI / 180);
                 bing.newY = bing.y + 20 * Math.sin((bing.end-parseInt(bing.end - bing.begin) / 2)*Math.PI / 180);
+                line.NBx = bing.newX;
+                line.NBy = bing.newY;
             } else {
                 bing.newX = 0;
                 bing.newY = 0;
+                line.NBx = bing.x;
+                line.NBy = bing.y;
             }
+
+
+
+
+
+
         }
     }
     drawBing();
@@ -101,6 +175,6 @@ function moveEvent(e) {
 
 
 window.onload = function () {
-    drawBing([{'name': '测试1', 'value': 3333}, {'name': '测试1', 'value': 5555},{'name': '测试3', 'value': 2342}, {'name': '测试3', 'value': 3212}])
+    drawBing([{'name': '测试1', 'value': 3333},{'name': '测试1', 'value': 2222}, {'name': '测试1', 'value': 5555},{'name': '测试3', 'value': 2342}, {'name': '测试3', 'value': 3212}])
     canvas.onmousedown = moveEvent;
 }
