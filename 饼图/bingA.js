b642ab26fb6354bf859b6be574aab1e7e751d6e4
Yuqian Gao <yuqian.gao@mljr.com>
2017-04-28 11:33:18 +0800

    判断点在扇形内

--- 饼图/bingA.js
+++ 饼图/bingA.js
@@ -44,10 +44,11 @@ function drawBing(arg) {
         g.lineTo(bArray[i].x, bArray[i].y);
         g.fillStyle = bArray[i].color;
         g.fill();
+        bArray[i].begin = temp*180;
+        bArray[i].end = ((bArray[i].value / total * 2) + temp)*180;
+
         temp = (bArray[i].value / total * 2) + temp;
-        if (g.isPointInPath(200, 240)) {//判断在哪个扇形内
-            console.log(bArray[i])
-        }
+        console.log(bArray)
     }
 }
 
@@ -57,6 +58,39 @@ function randomFromTo(from, to) {
 }//获取随机坐标
 
 
+function moveEvent(e) {
+    var mouseX = e.pageX - canvas.offsetLeft;
+    var mouseY = e.pageY - canvas.offsetTop;
+    //判断点是否在扇形上
+    for (var i in bArray) {
+        //判断在哪个圆内
+        var bing = bArray[i];
+        var distance = Math.sqrt(Math.pow(bing.x - mouseX, 2) + Math.pow(bing.y - mouseY, 2))
+        if (distance <= bing.radius) {
+
+            var XformO = mouseX-200;
+            var YformO =mouseY-200;
+            var angle;
+            var angleX = Math.atan(XformO/YformO)*180/Math.PI;
+            if(YformO>=0&&XformO>=0||YformO>=0&&XformO<0){//第一、二象限
+                 angle = 90-angleX;
+            }else if(YformO<=0&&XformO>=0||YformO<=0&&XformO<0){//第三、四象限
+                angle = 90 - angleX +180
+            }
+            if(angle>=bArray[i].begin&&(angle<=bArray[i].end)){
+               console.log(bArray[i])
+            }
+
+
+        }
+    }
+}
+function redraw(){
+    
+}
+
+
 window.onload = function () {
-    drawBing([{'name': '测试1', 'value': 3333}, {'name': '测试3', 'value': 1112}, {'name': '测试3', 'value': 2342}])
+    drawBing([{'name': '测试1', 'value': 3333},{'name': '测试3', 'value': 2342}])
+    canvas.onmousedown = moveEvent;
 }
