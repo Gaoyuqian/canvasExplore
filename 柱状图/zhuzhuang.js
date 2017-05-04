5e3bcf3209d059670ff780e5f9612685ce5e3be0
Yuqian Gao <yuqian.gao@mljr.com>
2017-05-04 19:12:47 +0800

    添加鼠标事件

--- 柱状图/zhuzhuang.js
+++ 柱状图/zhuzhuang.js
@@ -8,10 +8,25 @@ var test = {
     offset: {'x': 700, 'y': 400},
     rectWidth: 70,
     jianju: 100,
-    info: [{'name': 'test1', 'value': 22}, {'name': 'test1', 'value': 15}, {'name': 'test2', 'value': 37}]
+    beishu: 1,
+    info: [{'name': '淘宝', 'value': 220}, {'name': '美团', 'value': 158}, {'name': '百度', 'value': 378}]
 }
 var canvas = document.getElementById('zhuzhuang');
 var g = canvas.getContext('2d');
+var rectArray = [];
+
+
+function getEect(x, y, width, height, color, name, value) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.name = name;
+    this.value = value;
+    this.isSelect = false;
+}
+
 function drawZuobiao() {
     g.beginPath();
     g.translate(test.o.x, test.o.y);
@@ -26,14 +41,21 @@ function drawZuobiao() {
     drawRect();
 }
 
+
+function addRect() {
+    for (var i = 0; i < test.info.length; i++) {
+        var rect = new getEect((i + 1) * (test.rectWidth + test.jianju), -(test.info[i].value * test.beishu), test.rectWidth, test.info[i].value * test.beishu, 'green', test.info[i].name, test.info[i].value);
+        rectArray.push(rect);
+    }
+    console.log(rectArray);
+}
 function drawKedu() {
     g.beginPath();
-    var total = test.info.length;
-    for (var i = 1; i <= total; i++) {
-        g.moveTo(i * (test.rectWidth + test.jianju) + test.rectWidth, 0)
-        g.lineTo(i * (test.rectWidth + test.jianju) + test.rectWidth, -5)
-        g.moveTo(i * (test.rectWidth + test.jianju), 0)
-        g.lineTo(i * (test.rectWidth + test.jianju), -5)
+    for (var i = 0; i < rectArray.length; i++) {
+        g.moveTo(rectArray[i].x, 0)
+        g.lineTo(rectArray[i].x+rectArray[i].width, -5)
+        g.moveTo(rectArray[i].x+rectArray[i].width, 0)
+        g.lineTo(rectArray[i].x+rectArray[i].width, -5)
         g.strokeStyle = 'red';
         g.stroke();
     }
@@ -44,7 +66,7 @@ function drawKedu() {
         g.lineTo(-10, -i);
         g.font = '20px';
         g.fillStyle = 'black';
-        g.fillText(i/10, -35, -i + 5)
+        g.fillText(parseInt(i / test.beishu), -35, -i + 5)
     }
     g.lineWidth = '1';
     g.strokeStyle = 'green';
@@ -52,19 +74,42 @@ function drawKedu() {
 }
 
 function drawRect() {
-    var total = test.info.length;
-
-    for (var i = 0; i < total; i++) {
+    for (var i = 0; i < rectArray.length; i++) {
         g.beginPath();
-        g.fillStyle = 'green';
-        g.fillRect((i + 1) * (test.rectWidth + test.jianju), -(test.info[i].value*10), test.rectWidth, test.info[i].value*10)
+        g.fillStyle = rectArray[i].color;
+        g.fillRect( rectArray[i].x, rectArray[i].y, rectArray[i].width, rectArray[i].height);
         g.beginPath();
         g.fillStyle = 'grey';
         g.fillText(test.info[i].name, (i + 1) * (test.rectWidth + test.jianju) + (test.rectWidth - g.measureText(test.info[i].name).width) / 2, 15)
     }
+}
+
+
+/*添加鼠标事件*/
+
+function mousemoving(e){
+
+    var movX = e.pageX - canvas.offsetLeft-test.o.x;
+    var movY = e.pageY - canvas.offsetTop-test.o.y;
+
+
+    for(var i = 0;i<rectArray.length;i++){
+        //坐标计算错误
+        if((movX>rectArray[i].x&&movX<rectArray[i].x+rectArray[i].width)&&(movX>0&&movY<-rectArray[i].y)){
+            console.log(rectArray[i]);
+        }
+    }
+
+
 
 
 }
+
 window.onload = function () {
+    addRect();
     drawZuobiao();
+
+
+
+    window.onmousemove=mousemoving;
 }
