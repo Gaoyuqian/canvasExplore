a1ba67ab2aa56cde7fd8bde21aa7e8f5a24b9570
Yuqian Gao <yuqian.gao@mljr.com>
2017-05-05 10:51:20 +0800

    第二版

--- 柱状图/zhuzhuang.js
+++ 柱状图/zhuzhuang.js
@@ -6,24 +6,27 @@
 var test = {
     o: {'x': 100, 'y': 700},
     offset: {'x': 700, 'y': 400},
-    rectWidth: 70,
-    jianju: 100,
+    rectWidth: 100,
+    jianju: 10,
     beishu: 1,
-    info: [{'name': '淘宝', 'value': 220}, {'name': '美团', 'value': 158}, {'name': '百度', 'value': 378}]
+    info: [{'name': '淘宝', 'value': 220}, {'name': '淘宝', 'value': 220}, {'name': '淘宝', 'value': 220}, {
+        'name': '淘宝',
+        'value': 220
+    }, {'name': '美团', 'value': 158}, {'name': '百度', 'value': 378}]
 }
 var canvas = document.getElementById('zhuzhuang');
 var g = canvas.getContext('2d');
 var rectArray = [];
 
 
-function getEect(x, y, width, height, color, name, value) {
+function getEect(x, y, width, height, color, name) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.color = color;
     this.name = name;
-    this.value = value;
+    this.value ='';
     this.isSelect = false;
 }
 
@@ -44,7 +47,7 @@ function drawZuobiao() {
 
 function addRect() {
     for (var i = 0; i < test.info.length; i++) {
-        var rect = new getEect((i + 1) * (test.rectWidth + test.jianju), -(test.info[i].value * test.beishu), test.rectWidth, test.info[i].value * test.beishu, 'green', test.info[i].name, test.info[i].value);
+        var rect = new getEect((i + 1) * (test.rectWidth + test.jianju), -(test.info[i].value * test.beishu), test.rectWidth, test.info[i].value * test.beishu, 'green', test.info[i].name);
         rectArray.push(rect);
     }
     console.log(rectArray);
@@ -53,9 +56,9 @@ function drawKedu() {
     g.beginPath();
     for (var i = 0; i < rectArray.length; i++) {
         g.moveTo(rectArray[i].x, 0)
-        g.lineTo(rectArray[i].x+rectArray[i].width, -5)
-        g.moveTo(rectArray[i].x+rectArray[i].width, 0)
-        g.lineTo(rectArray[i].x+rectArray[i].width, -5)
+        g.lineTo(rectArray[i].x + rectArray[i].width, -5)
+        g.moveTo(rectArray[i].x + rectArray[i].width, 0)
+        g.lineTo(rectArray[i].x + rectArray[i].width, -5)
         g.strokeStyle = 'red';
         g.stroke();
     }
@@ -73,34 +76,52 @@ function drawKedu() {
     g.stroke();
 }
 
-function drawRect() {
+function drawRect(first) {
     for (var i = 0; i < rectArray.length; i++) {
         g.beginPath();
-        g.fillStyle = rectArray[i].color;
-        g.fillRect( rectArray[i].x, rectArray[i].y, rectArray[i].width, rectArray[i].height);
-        g.beginPath();
-        g.fillStyle = 'grey';
-        g.fillText(test.info[i].name, (i + 1) * (test.rectWidth + test.jianju) + (test.rectWidth - g.measureText(test.info[i].name).width) / 2, 15)
+        g.fillStyle = !rectArray[i].isSelect ? rectArray[i].color : 'red';
+        g.fillRect(rectArray[i].x, rectArray[i].y, rectArray[i].width, rectArray[i].height);
+
+
+
+
+        //g.beginPath();
+        //g.fillStyle = 'black'
+        //g.fillText(rectArray[i].value, rectArray[i].x + (rectArray[i].width-g.measureText(rectArray[i].x).width)/2, rectArray[i].y - 10)
+
+
+
+
+        if (!first) {
+            g.beginPath();
+            g.fillStyle = 'grey';
+            g.fillText(test.info[i].name, (i + 1) * (test.rectWidth + test.jianju) + (test.rectWidth - g.measureText(test.info[i].name).width) / 2, 15)
+        }
     }
 }
 
 
-/*添加鼠标事件*/
 
-function mousemoving(e){
+function mousemoving(e) {
 
-    var movX = e.pageX - canvas.offsetLeft-test.o.x;
-    var movY = e.pageY - canvas.offsetTop-test.o.y;
+    var movX = Math.abs(e.pageX - canvas.offsetLeft - test.o.x);
+    var movY = Math.abs(e.pageY - canvas.offsetTop - test.o.y);
 
 
-    for(var i = 0;i<rectArray.length;i++){
+    for (var i = 0; i < rectArray.length; i++) {
         //坐标计算错误
-        if((movX>rectArray[i].x&&movX<rectArray[i].x+rectArray[i].width)&&(movX>0&&movY<-rectArray[i].y)){
-            console.log(rectArray[i]);
+        rectArray[i].isSelect = false;
+
+        if ((movX > rectArray[i].x && movX < rectArray[i].x + rectArray[i].width) && (movX > 0 && movY < -rectArray[i].y)) {
+
+
+            rectArray[i].isSelect = true;
+            rectArray[i].value =  rectArray[i].height;
+
         }
+
+        drawRect(true);
     }
-
-
 
 
 }
@@ -110,6 +131,5 @@ window.onload = function () {
     drawZuobiao();
 
 
-
-    window.onmousemove=mousemoving;
+    window.onmousemove = mousemoving;
 }
